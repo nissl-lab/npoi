3f388d5f7161de6d283300b1674803dde1a69f99
tonyqus@gmail.com <tonyqus@gmail.com@3d0cb27c-5942-9f1f-b0c8-b51bd5f61ba7>
2011-10-31 01:09:08 +0000

    implement short type for FillPatternType
    git-svn-id: http://npoi.googlecode.com/svn/trunk@31 3d0cb27c-5942-9f1f-b0c8-b51bd5f61ba7

--- main/SS/UserModel/FillPatternType.cs
+++ main/SS/UserModel/FillPatternType.cs
@@ -22,65 +22,65 @@ namespace NPOI.SS.UserModel
      * The enumeration value indicating the style of fill pattern being used for a cell format.
      * 
      */
-    public enum FillPatternType
+    public enum FillPatternType : short
     {
 
         /**  No background */
-        NO_FILL,
+        NO_FILL = 0,
 
         /**  Solidly Filled */
-        SOLID_FOREGROUND,
+        SOLID_FOREGROUND = 1,
 
         /**  Small fine dots */
-        FINE_DOTS,
+        FINE_DOTS = 2,
 
         /**  Wide dots */
-        ALT_BARS,
+        ALT_BARS = 3,
 
         /**  Sparse dots */
-        SPARSE_DOTS,
+        SPARSE_DOTS = 4,
 
         /**  Thick horizontal bands */
-        THICK_HORZ_BANDS,
+        THICK_HORZ_BANDS = 5,
 
         /**  Thick vertical bands */
-        THICK_VERT_BANDS,
+        THICK_VERT_BANDS = 6,
 
         /**  Thick backward facing diagonals */
-        THICK_BACKWARD_DIAG,
+        THICK_BACKWARD_DIAG = 7,
 
         /**  Thick forward facing diagonals */
-        THICK_FORWARD_DIAG,
+        THICK_FORWARD_DIAG = 8,
 
         /**  Large spots */
-        BIG_SPOTS,
+        BIG_SPOTS = 9,
 
         /**  Brick-like layout */
-        BRICKS,
+        BRICKS = 10,
 
         /**  Thin horizontal bands */
-        THIN_HORZ_BANDS,
+        THIN_HORZ_BANDS = 11,
 
         /**  Thin vertical bands */
-        THIN_VERT_BANDS,
+        THIN_VERT_BANDS = 12,
 
         /**  Thin backward diagonal */
-        THIN_BACKWARD_DIAG,
+        THIN_BACKWARD_DIAG = 13,
 
         /**  Thin forward diagonal */
-        THIN_FORWARD_DIAG,
+        THIN_FORWARD_DIAG = 14,
 
         /**  Squares */
-        SQUARES,
+        SQUARES = 15,
 
         /**  Diamonds */
-        DIAMONDS,
+        DIAMONDS = 16,
 
         /**  Less Dots */
-        LESS_DOTS,
+        LESS_DOTS = 17,
 
         /**  Least Dots */
-        LEAST_DOTS
+        LEAST_DOTS = 18
 
     }
 }
