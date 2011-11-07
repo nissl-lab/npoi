27973337e89f42019098f0125ac7bc2b08762b1c
tonyqus <tonyqus@3d0cb27c-5942-9f1f-b0c8-b51bd5f61ba7>
2011-11-07 06:27:51 +0000

    add HWPF testcases
    git-svn-id: http://npoi.googlecode.com/svn/trunk@42 3d0cb27c-5942-9f1f-b0c8-b51bd5f61ba7

--- testcases/scratchpad/Properties/AssemblyInfo.cs
+++ testcases/scratchpad/Properties/AssemblyInfo.cs
@@ -33,3 +33,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: InternalsVisibleTo("TestCases")]
