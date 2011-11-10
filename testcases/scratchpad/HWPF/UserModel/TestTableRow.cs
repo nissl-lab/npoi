e576b34fe5e120fce000ece56f2ca81562346329
tonyqus <tonyqus@3d0cb27c-5942-9f1f-b0c8-b51bd5f61ba7>
2011-11-10 08:35:47 +0000

    add test cases
    git-svn-id: http://npoi.googlecode.com/svn/trunk@48 3d0cb27c-5942-9f1f-b0c8-b51bd5f61ba7

---
+++ testcases/scratchpad/HWPF/UserModel/TestTableRow.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NPOI.HWPF;
+using NPOI.HWPF.UserModel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestCases.HWPF.UserModel
+{
+    [TestClass]
+    public class TestTableRow
+    {
+        [TestMethod]
+        public void TestInnerTableCellsDetection()
+        {
+            HWPFDocument hwpfDocument = new HWPFDocument(POIDataSamples
+                    .GetDocumentInstance().OpenResourceAsStream("innertable.doc"));
+            hwpfDocument.GetRange();
+
+            Range documentRange = hwpfDocument.GetRange();
+            Paragraph startOfInnerTable = documentRange.GetParagraph(6);
+
+            Table innerTable = documentRange.GetTable(startOfInnerTable);
+            Assert.AreEqual(2, innerTable.NumRows);
+
+            TableRow tableRow = innerTable.GetRow(0);
+            Assert.AreEqual(2, tableRow.NumCells());
+        }
+        [TestMethod]
+        public void TestOuterTableCellsDetection()
+        {
+            HWPFDocument hwpfDocument = new HWPFDocument(POIDataSamples
+                    .GetDocumentInstance().OpenResourceAsStream("innertable.doc"));
+            hwpfDocument.GetRange();
+
+            Range documentRange = hwpfDocument.GetRange();
+            Paragraph startOfOuterTable = documentRange.GetParagraph(0);
+
+            Table outerTable = documentRange.GetTable(startOfOuterTable);
+            Assert.AreEqual(3, outerTable.NumRows);
+
+            Assert.AreEqual(3, outerTable.GetRow(0).NumCells());
+            Assert.AreEqual(3, outerTable.GetRow(1).NumCells());
+            Assert.AreEqual(3, outerTable.GetRow(2).NumCells());
+        }
+
+    }
+
+}
