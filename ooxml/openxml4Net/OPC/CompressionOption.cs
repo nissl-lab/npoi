e9c69dc5c3d00fb40d628771e8879cff23eaa4c1
tonyqus@gmail.com <tonyqus@gmail.com@3d0cb27c-5942-9f1f-b0c8-b51bd5f61ba7>
2011-12-06 01:07:56 +0000

    add CompressionOption
    git-svn-id: http://npoi.googlecode.com/svn/trunk@113 3d0cb27c-5942-9f1f-b0c8-b51bd5f61ba7

---
+++ ooxml/openxml4Net/OPC/CompressionOption.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ICSharpCode.SharpZipLib.Zip.Compression;
+
+namespace NPOI.OpenXml4Net.OPC
+{
+    public enum CompressionOption : int
+    {
+        Fast = Deflater.BEST_SPEED,
+        Maximum = Deflater.BEST_COMPRESSION,
+        Normal = Deflater.DEFAULT_COMPRESSION,
+        NotCompressed = Deflater.NO_COMPRESSION
+    }
+}
