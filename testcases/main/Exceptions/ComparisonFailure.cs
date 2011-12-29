4371ba900409f8c2822329207611d6a6b8a74ef6
antony.apollo@gmail.com <antony.apollo@gmail.com@3d0cb27c-5942-9f1f-b0c8-b51bd5f61ba7>
2011-12-29 04:01:28 +0000

    simulate class ComparisonFailure in java
    git-svn-id: http://npoi.googlecode.com/svn/trunk@186 3d0cb27c-5942-9f1f-b0c8-b51bd5f61ba7

---
+++ testcases/main/Exceptions/ComparisonFailure.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestCases.Exceptions
+{
+    //simulate java class ComparisonFailure
+    public class ComparisonFailure: ApplicationException
+    {
+        public ComparisonFailure(string message, string expected, string actual):
+            base(string.Format("{0} excepted:{1} actual:{2}", message, expected, actual))
+        {
+
+        }
+    }
+}
