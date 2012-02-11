60fa53569820c90bc9852d812a6bcaa8b0edfc8a
antony.apollo@gmail.com <antony.apollo@gmail.com@3d0cb27c-5942-9f1f-b0c8-b51bd5f61ba7>
2012-02-11 06:25:18 +0000

    add files in NPOI.POIFS.NIO
    git-svn-id: http://npoi.googlecode.com/svn/trunk@263 3d0cb27c-5942-9f1f-b0c8-b51bd5f61ba7

---
+++ main/POIFS/NIO/ByteArrayBackedDataSource.cs
@@ -0,0 +1,110 @@
+﻿/* ====================================================================
+   Licensed to the Apache Software Foundation (ASF) under one or more
+   contributor license agreements.  See the NOTICE file distributed with
+   this work for additional information regarding copyright ownership.
+   The ASF licenses this file to You under the Apache License, Version 2.0
+   (the "License"); you may not use this file except in compliance with
+   the License.  You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+==================================================================== */
+
+using System.IO;
+using System;
+namespace NPOI.POIFS.NIO
+{
+    /// <summary>
+    /// A POIFS <see cref="T:NPOI.POIFS.NIO.DataSource"/> backed by a byte array.
+    /// </summary>
+    public class ByteArrayBackedDataSource : DataSource
+    {
+        private byte[] buffer;
+        private long size;
+        
+        public ByteArrayBackedDataSource(byte[] data, int size)
+        {
+            this.buffer = data;
+            this.size = size;
+        }
+        public ByteArrayBackedDataSource(byte[] data)
+            : this(data, data.Length)
+        {
+        }
+
+        public override MemoryStream Read(int length, long position)
+        {
+            if (position >= size)
+            {
+                throw new IndexOutOfRangeException(
+                      "Unable to read " + length + " bytes from " +
+                      position + " in stream of length " + size
+                );
+            }
+
+            int toRead = (int)Math.Min(length, size - position);
+            return new MemoryStream(buffer, (int)position, toRead);
+        }
+
+        public override void Write(MemoryStream src, long position)
+        {
+            // Extend if needed
+            long endPosition = position + src.Length;
+            if (endPosition > buffer.Length)
+            {
+                Extend(endPosition);
+            }
+
+            // Now copy
+            src.Read(buffer, (int)position, (int)src.Length);
+
+            // Update size if needed
+            if (endPosition > size)
+            {
+                size = endPosition;
+            }
+        }
+
+        private void Extend(long length)
+        {
+            // Consider extending by a bit more than requested
+            long difference = length - buffer.Length;
+            if (difference < buffer.Length * 0.25)
+            {
+                difference = (long)(buffer.Length * 0.25);
+            }
+            if (difference < 4096)
+            {
+                difference = 4096;
+            }
+
+            byte[] nb = new byte[(int)(difference + buffer.Length)];
+            Array.Copy(buffer, 0, nb, 0, (int)size);
+            buffer = nb;
+        }
+
+        public override void CopyTo(Stream stream)
+        {
+            stream.Write(buffer, 0, (int)size);
+        }
+
+        public override long Size
+        {
+            get
+            {
+                return size;
+            }
+        }
+
+        public override void Close()
+        {
+            buffer = null;
+            size = -1;
+        }
+    }
+}
---
+++ main/POIFS/NIO/FileBackedDataSource.cs
@@ -0,0 +1,113 @@
+﻿/* ====================================================================
+   Licensed to the Apache Software Foundation (ASF) under one or more
+   contributor license agreements.  See the NOTICE file distributed with
+   this work for additional information regarding copyright ownership.
+   The ASF licenses this file to You under the Apache License, Version 2.0
+   (the "License"); you may not use this file except in compliance with
+   the License.  You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+==================================================================== */
+
+using System.IO;
+using System;
+using NPOI.Util;
+namespace NPOI.POIFS.NIO
+{
+    /**
+ * A POIFS {@link DataSource} backed by a File
+ */
+    public class FileBackedDataSource : DataSource
+    {
+        private FileStream fileStream;
+
+        public FileBackedDataSource(string file)
+        {
+            if (!File.Exists(file))
+            {
+                throw new FileNotFoundException(file);
+            }
+            this.fileStream = new FileStream(file, FileMode.Open);
+        }
+        public FileBackedDataSource(FileStream channel)
+        {
+            this.fileStream = channel;
+        }
+        /// <summary>
+        /// Reads a sequence of bytes from this FileStream starting at the given file position.
+        /// </summary>
+        /// <param name="length"></param>
+        /// <param name="position">The file position at which the transfer is to begin;</param>
+        /// <returns></returns>
+        public override MemoryStream Read(int length, long position)
+        {
+            if (position >= Size)
+            {
+                throw new ArgumentException("Position " + position + " past the end of the file");
+            }
+
+            // Read
+            fileStream.Position = position;
+            byte[] buffer = new byte[length];
+            int worked = IOUtils.ReadFully(fileStream, buffer);
+
+            // Check
+            if (worked == -1)
+            {
+                throw new ArgumentException("Position " + position + " past the end of the file");
+            }
+            MemoryStream dst = new MemoryStream(buffer);
+            // Ready it for reading
+            dst.Position = 0;
+
+            // All done
+            return dst;
+        }
+        /// <summary>
+        /// Writes a sequence of bytes to this FileStream from the given Stream,
+        /// starting at the given file position.
+        /// </summary>
+        /// <param name="src">The Stream from which bytes are to be transferred</param>
+        /// <param name="position">The file position at which the transfer is to begin;
+        /// must be non-negative</param>
+        public override void Write(MemoryStream src, long position)
+        {
+            src.Seek(0, SeekOrigin.Begin);
+            byte[] buffer = src.GetBuffer();
+            fileStream.Position = position;
+            fileStream.Write(buffer, 0, buffer.Length);
+        }
+
+        public override void CopyTo(Stream stream)
+        {
+            // Wrap the OutputSteam as a channel
+            //WritableByteChannel out = Channels.newChannel(stream);
+            // Now do the transfer
+            //channel.transferTo(0, channel.size(), out);
+            fileStream.Position = 0;
+            byte[] buffer = new byte[fileStream.Length];
+            fileStream.Read(buffer, 0, buffer.Length);
+            stream.Write(buffer, 0, buffer.Length);
+        }
+
+        public override long Size
+        {
+            get
+            {
+                return fileStream.Length;
+            }
+        }
+
+        public override void Close()
+        {
+            fileStream.Close();
+        }
+    }
+
+}
