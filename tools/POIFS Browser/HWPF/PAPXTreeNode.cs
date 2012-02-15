b67c368c276c6f464e657b37caef2ea1e5e36cd6
tonyqus@gmail.com <tonyqus@gmail.com@3d0cb27c-5942-9f1f-b0c8-b51bd5f61ba7>
2012-02-15 21:58:08 +0000

    checkin HWPF treenode for POIFS browser to fix the build
    git-svn-id: http://npoi.googlecode.com/svn/trunk@282 3d0cb27c-5942-9f1f-b0c8-b51bd5f61ba7

---
+++ tools/POIFS Browser/HWPF/PAPXTreeNode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NPOI.HWPF.Model;
+
+namespace NPOI.Tools.POIFSBrowser.HWPF
+{
+    public class PAPXTreeNode:AbstractHWPFTreeNode
+    {
+        public PAPXTreeNode(PAPX papx)
+        {
+            this.Record = papx;
+        }
+
+        public override byte[] GetBytes()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
