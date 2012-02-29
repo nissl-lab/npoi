dc7a78fe707cdf1206142a4976f0578d5e0347b1
christian.leutloff@gmail.com <christian.leutloff@gmail.com@3d0cb27c-5942-9f1f-b0c8-b51bd5f61ba7>
2012-02-29 12:45:58 +0000

    Added project page to AssemblyInfo.cs.
    git-svn-id: http://npoi.googlecode.com/svn/trunk@338 3d0cb27c-5942-9f1f-b0c8-b51bd5f61ba7

--- main/Properties/AssemblyInfo.cs
+++ main/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Security;
 [assembly: AssemblyTitle("NPOI")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("NPOI team")]
+[assembly: AssemblyCompany("NPOI team, https://code.google.com/p/npoi/")]
 [assembly: AssemblyProduct("NPOI")]
 [assembly: AssemblyCopyright("Apache 2.0")]
 [assembly: AssemblyTrademark("NPOI")]
