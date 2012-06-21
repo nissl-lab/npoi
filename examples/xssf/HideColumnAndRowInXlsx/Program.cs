b42742988aafbc3aa04c3c8f9d89ab41a25db2d1
tonyqus@gmail.com <tonyqus@gmail.com@3d0cb27c-5942-9f1f-b0c8-b51bd5f61ba7>
2012-06-21 03:51:53 +0000

    add new examples for NPOI.XSSF: HideColumnAndRowInXlsx  and SplitAndFreezePanesInXlsx
    git-svn-id: http://npoi.googlecode.com/svn/trunk@483 3d0cb27c-5942-9f1f-b0c8-b51bd5f61ba7

---
+++ examples/xssf/HideColumnAndRowInXlsx/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NPOI.XSSF.UserModel;
+using NPOI.SS.UserModel;
+using System.IO;
+
+namespace NPOI.Examples.XSSF.HideColumnAndRowInXlsx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWorkbook workbook = new XSSFWorkbook();
+            ISheet s = workbook.CreateSheet("Sheet1");
+            IRow r1 = s.CreateRow(0);
+            IRow r2 = s.CreateRow(1);
+            IRow r3 = s.CreateRow(2);
+            IRow r4 = s.CreateRow(3);
+            IRow r5 = s.CreateRow(4);
+
+            //hide IRow 2
+            r2.ZeroHeight = true;
+
+            //hide column C
+            s.SetColumnHidden(2, true);
+            FileStream sw = File.OpenWrite("test.xlsx");
+            workbook.Write(sw);
+            sw.Close();
+        }
+    }
+}
