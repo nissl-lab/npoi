a22ba611ca97a1ea76c268bd7b25080470d8936a
tonyqus <tonyqus@3d0cb27c-5942-9f1f-b0c8-b51bd5f61ba7>
2012-08-05 07:15:05 +0000

    Add XSSF examples: AddHyperlinkInXlsx, ApplyFontInXlsx, MeringCellsInXlsx, PageSetupInXlsx
    git-svn-id: http://npoi.googlecode.com/svn/trunk@518 3d0cb27c-5942-9f1f-b0c8-b51bd5f61ba7

---
+++ examples/xssf/AddHyperlinkInXlsx/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NPOI.XSSF.UserModel;
+using NPOI.SS.UserModel;
+using System.IO;
+using NPOI.HSSF.Util;
+using NPOI.HSSF.UserModel;
+
+namespace NPOI.Examples.XSSF.AddHyperlinkInXlsx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWorkbook workbook = new XSSFWorkbook();
+            ////cell style for hyperlinks
+            ////by default hyperlinks are blue and underlined
+            ICellStyle hlink_style = workbook.CreateCellStyle();
+            IFont hlink_font = workbook.CreateFont();
+            hlink_font.Underline = (byte)FontUnderlineType.SINGLE;
+            hlink_font.Color = HSSFColor.BLUE.index;
+            hlink_style.SetFont(hlink_font);
+
+            ICell cell;
+            ISheet sheet = workbook.CreateSheet("Hyperlinks");
+
+            //URL
+            cell = sheet.CreateRow(0).CreateCell(0);
+            cell.SetCellValue("URL Link");
+            XSSFHyperlink link = new XSSFHyperlink(HyperlinkType.URL);
+            link.Address = ("http://poi.apache.org/");
+            cell.Hyperlink = (link);
+            cell.CellStyle = (hlink_style);
+
+            //link to a file in the current directory
+            cell = sheet.CreateRow(1).CreateCell(0);
+            cell.SetCellValue("File Link");
+            link = new XSSFHyperlink(HyperlinkType.FILE);
+            link.Address = ("link1.xls");
+            cell.Hyperlink = (link);
+            cell.CellStyle = (hlink_style);
+
+            //e-mail link
+            cell = sheet.CreateRow(2).CreateCell(0);
+            cell.SetCellValue("Email Link");
+            link = new XSSFHyperlink(HyperlinkType.EMAIL);
+            //note, if subject contains white spaces, make sure they are url-encoded
+            link.Address = ("mailto:poi@apache.org?subject=Hyperlinks");
+            cell.Hyperlink = (link);
+            cell.CellStyle = (hlink_style);
+
+            //link to a place in this workbook
+
+            //Create a target sheet and cell
+            ISheet sheet2 = workbook.CreateSheet("Target ISheet");
+            sheet2.CreateRow(0).CreateCell(0).SetCellValue("Target ICell");
+
+            cell = sheet.CreateRow(3).CreateCell(0);
+            cell.SetCellValue("Worksheet Link");
+            link = new XSSFHyperlink(HyperlinkType.DOCUMENT);
+            link.Address = ("'Target ISheet'!A1");
+            cell.Hyperlink = (link);
+            cell.CellStyle = (hlink_style);
+
+            FileStream sw = File.Create("test.xlsx");
+            workbook.Write(sw);
+            sw.Close();
+        }
+    }
+}
---
+++ examples/xssf/MeringCellsInXlsx/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NPOI.XSSF.UserModel;
+using NPOI.SS.UserModel;
+using System.IO;
+using NPOI.SS.Util;
+
+namespace NPOI.Examples.XSSF.MeringCellsInXlsx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWorkbook workbook = new XSSFWorkbook();
+            ISheet sheet = workbook.CreateSheet("PictureSheet");
+
+            IRow row = sheet.CreateRow(1);
+            ICell cell = row.CreateCell(1);
+            cell.SetCellValue(new XSSFRichTextString("This is a test of merging"));
+
+            sheet.AddMergedRegion(new CellRangeAddress(1, 1, 1, 2));
+
+            FileStream sw = File.OpenWrite("test.xlsx");
+            workbook.Write(sw);
+            sw.Close();
+        }
+    }
+}
