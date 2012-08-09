dbbf551dd2dfac037eb928cf9bb9d7261b0daf69
tonyqus <tonyqus@3d0cb27c-5942-9f1f-b0c8-b51bd5f61ba7>
2012-08-09 00:43:43 +0000

    change version info; update .net 2.0 test cases
    git-svn-id: http://npoi.googlecode.com/svn/trunk@521 3d0cb27c-5942-9f1f-b0c8-b51bd5f61ba7

--- ooxml/Properties/AssemblyInfoDotnet2.cs
+++ ooxml/Properties/AssemblyInfoDotnet2.cs
@@ -6,7 +6,7 @@ using System.Security;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NPOI")]
+[assembly: AssemblyTitle("NPOI OOXML")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("NPOI team, https://code.google.com/p/npoi/")]
@@ -21,7 +21,7 @@ using System.Security;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("9bf85b73-8802-42ad-b83f-0ab48190b835")]
+[assembly: Guid("cc03d84d-498a-4561-97c1-e39d5d7780a0")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -35,5 +35,7 @@ using System.Security;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2.0.0.0")]
 [assembly: AssemblyFileVersion("2.0.0.0")]
-[assembly: InternalsVisibleTo("NPOI.TestCases, PublicKey=002400000480000094000000060200000024000052534131000400000100010095ccd95af3b39d8bc20544d3f47fd24b53ebc5ccb693eaed116290629f8cd882c827ebd511ad59449224f0718d3f9d03b64945a6c8b6644266001b8c8426185330e3d96da70ae16d4acc21b8d4d480f1385c7e924273179375aa88f81380a72fb115712a313379d16aed4aa36208ee3b4a5dd785b06a07b2d868e3227f4495b5", AllInternalsVisible = true)]
+//[assembly: InternalsVisibleTo("ooxml.Testcases")]
+
+[assembly: InternalsVisibleTo("NPOI.OOXML.TestCases", AllInternalsVisible = true)]
 [assembly: AllowPartiallyTrustedCallers]
