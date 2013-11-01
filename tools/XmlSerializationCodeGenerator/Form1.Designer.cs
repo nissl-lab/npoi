d3cc29df7976ed4d6016faa55a5f1df685f441af
Tony Qu <tonyqus@gmail.com>
2013-11-02 00:43:23 +0800

    support generating properties based on Items attributes

--- tools/XmlSerializationCodeGenerator/Form1.Designer.cs
+++ tools/XmlSerializationCodeGenerator/Form1.Designer.cs
@@ -33,6 +33,7 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // treeView1
@@ -64,7 +65,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(437, 10);
+            this.button2.Location = new System.Drawing.Point(369, 11);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(128, 32);
             this.button2.TabIndex = 3;
@@ -74,7 +75,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(647, 12);
+            this.button3.Location = new System.Drawing.Point(676, 11);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(83, 31);
             this.button3.TabIndex = 4;
@@ -82,11 +83,22 @@
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(518, 11);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(102, 32);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "生成属性代码";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(855, 541);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox1);
@@ -106,6 +118,7 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
