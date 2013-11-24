0cfd4ab9cdd56162d0697284e1692739233817db
Tony Qu <tonyqus@gmail.com>
2013-11-24 18:59:21 +0800

    Update version to 2.0.5
    NPOI 2.0 beta 2 version

--- ooxml/OpenXmlFormats/Properties/AssemblyInfo.cs
+++ ooxml/OpenXmlFormats/Properties/AssemblyInfo.cs
@@ -33,7 +33,7 @@ using System.Security;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyVersion("2.0.5.0")]
 [assembly: AssemblyFileVersion("2.0.5.0")]
 [assembly: AssemblyInformationalVersion("NPOI 2.0 beta2")]
 [assembly: AllowPartiallyTrustedCallers]
