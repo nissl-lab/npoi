a15b005796b21e82911c7258ab791c5fc0d73659
Tony Qu <tonyqus@gmail.com>
2013-11-25 16:17:03 +0800

    add new example: CreateCustomProperties for XSSF

---
+++ examples/xssf/CreateCustomProperties/Program.cs
@@ -0,0 +1,29 @@
+﻿using NPOI;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace CreateCustomProperties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XSSFWorkbook workbook = new XSSFWorkbook();
+            ISheet sheet1 = workbook.CreateSheet("Sheet1");
+
+            POIXMLProperties props = workbook.GetProperties();
+            props.GetCoreProperties().SetCreator("NPOI 2.0.5");
+            props.GetCoreProperties().SetCreated(DateTime.Now);
+            props.GetCustomProperties().AddProperty("NPOI Team", "Hello World!");
+
+            FileStream sw = File.Create("test.xlsx");
+            workbook.Write(sw);
+            sw.Close();
+        }
+    }
+}
