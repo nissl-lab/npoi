64b2bf7a748837d88efc3cac71454b3ee6486c10
antony-liu <sun_apollo@yeah.net>
2015-06-12 20:44:29 +0800

    Bugzilla 54625 - Register user-defined functions in instance scope instead of static

--- main/HSSF/UserModel/HSSFWorkbook.cs
+++ main/HSSF/UserModel/HSSFWorkbook.cs
@@ -1362,7 +1362,7 @@ namespace NPOI.HSSF.UserModel
  * By default includes functions from the Excel Analysis Toolpack
  */
         [NonSerialized]
-        private UDFFinder _udfFinder = UDFFinder.DEFAULT;
+        private UDFFinder _udfFinder = new IndexedUDFFinder(UDFFinder.DEFAULT);
 
         /**
  * Register a new toolpack in this workbook.
