ac06dd725948904fe9b1206a18d1b8427148fbd6
antony-liu <sun_apollo@yeah.net>
2015-06-14 18:38:27 +0800

    Bug 55047: REPT formula test case

--- testcases/main/SS/Formula/Functions/TestReptFunctionsFromSpreadsheet.cs
+++ testcases/main/SS/Formula/Functions/TestReptFunctionsFromSpreadsheet.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using NUnit.Framework;
 
 namespace TestCases.SS.Formula.Functions
 {
+    [TestFixture]
     public class TestReptFunctionsFromSpreadsheet:BaseTestFunctionsFromSpreadsheet
     {
         protected override string Filename
