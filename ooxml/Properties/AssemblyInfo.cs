febe3dc8561dd6b9cf5cb6a0c4f3ebff04009e49
Tony Qu <tonyqus@163.com>
2015-12-18 11:53:30 +0800

    change assembly version to 2.2.0.2

--- ooxml/Properties/AssemblyInfo.cs
+++ ooxml/Properties/AssemblyInfo.cs
@@ -33,8 +33,8 @@ using System.Security;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below: 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.2.0.0")]
-[assembly: AssemblyFileVersion("2.2.0.0")]
+[assembly: AssemblyVersion("2.2.0.2")]
+[assembly: AssemblyFileVersion("2.2.0.2")]
 [assembly: AssemblyInformationalVersion("NPOI 2.2")]
 
 //[assembly: InternalsVisibleTo("ooxml.Testcases")]
