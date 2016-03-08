6139c30ce41f9131a14a4a2d144c46d64d0abfa6
Tony Qu <tonyqus@163.com>
2016-03-08 22:37:23 +0800

    fix InsertPicturesInXlsx example

--- examples/xssf/InsertPicturesInXlsx/Program.cs
+++ examples/xssf/InsertPicturesInXlsx/Program.cs
@@ -18,7 +18,7 @@ namespace NPOI.Examples.XSSF.InsertPicturesInXlsx
             IDrawing patriarch = sheet1.CreateDrawingPatriarch();
             //create the anchor
             XSSFClientAnchor anchor = new XSSFClientAnchor(500, 200, 0, 0, 2, 2, 4, 7);
-            anchor.AnchorType = 2;
+            anchor.AnchorType =  AnchorType.MoveDontResize;
             //load the picture and get the picture index in the workbook
             //first picture
             int imageId= LoadImage("../../image/HumpbackWhale.jpg", workbook);
