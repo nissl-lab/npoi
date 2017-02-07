dad660fbca2a2105474f75c699ec2e3dbe470b8a
Rye <Rye@DESKTOP-RYE>
2017-02-07 13:46:11 -0800

    Add apache license header

--- ooxml/XSSF/Streaming/SXSSFRow.cs
+++ ooxml/XSSF/Streaming/SXSSFRow.cs
@@ -1,4 +1,20 @@
-﻿using System;
+﻿/* ====================================================================
+   Licensed to the Apache Software Foundation (ASF) under one or more
+   contributor license agreements.  See the NOTICE file distributed with
+   this work for Additional information regarding copyright ownership.
+   The ASF licenses this file to You under the Apache License, Version 2.0
+   (the "License"); you may not use this file except in compliance with
+   the License.  You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+==================================================================== */
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,10 +32,7 @@ namespace NPOI.XSSF.Streaming
         private static bool? UNDEFINED = null;
 
         public SXSSFSheet _sheet; // parent sheet
-        //TODO: replacing with dict to get compling may need to alter for performance.
         public SortedDictionary<int,SXSSFCell> _cells = new SortedDictionary<int, SXSSFCell>();
-        //public List<Tuple<int,SXSSFCell>> _cells = new List<Tuple<int, SXSSFCell>>();
-        //private SortedMap<Integer, SXSSFCell> _cells = new TreeMap<Integer, SXSSFCell>();
         public short _style = -1; // index of cell style in style table
         public short _height = -1; // row height in twips (1/20 point)
         public bool _zHeight = false; // row zero-height (this is somehow different than being hidden)
