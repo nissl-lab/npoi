5bdb03ba22b29f4a98e946d7156bfa299168a054
Tony Qu <tony.qu@neuzilla.com>
2017-03-20 18:57:00 +0800

    update NPOI version

--- ooxml/OpenXmlFormats/Properties/AssemblyInfo.cs
+++ ooxml/OpenXmlFormats/Properties/AssemblyInfo.cs
@@ -33,7 +33,7 @@ using System.Security;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.2.1.0")]
-[assembly: AssemblyFileVersion("2.2.1.0")]
+[assembly: AssemblyVersion("2.3.0.0")]
+[assembly: AssemblyFileVersion("2.3.0.0")]
 [assembly: AssemblyInformationalVersion("2.0.0.0")]
 [assembly: AllowPartiallyTrustedCallers]
