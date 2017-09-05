da0aecc9f69197564da7b2b3a9b6fa3b90ccbc90
Andrey.Tretjakov <tretjakov.a@skbkontur.ru>
2017-09-05 12:56:24 +0500

    Bug 47245: Comment out check and exception in case row-records are missing for some cells as it seems POI is handling this gracefully and LibreOffice/Excel are as well

--- main/HSSF/UserModel/HSSFSheet.cs
+++ main/HSSF/UserModel/HSSFSheet.cs
@@ -185,13 +185,14 @@ namespace NPOI.HSSF.UserModel
                     hrow = (HSSFRow)GetRow(cval.Row);
                     if (hrow == null)
                     {
+                        /* we removed this check, see bug 47245 for the discussion around this
                         // Some tools (like Perl module SpReadsheet::WriteExcel - bug 41187) skip the RowRecords 
                         // Excel, OpenOffice.org and GoogleDocs are all OK with this, so POI should be too.
                         if (rowRecordsAlreadyPresent)
                         {
                             // if at least one row record is present, all should be present.
                             throw new Exception("Unexpected missing row when some rows already present, the file is wrong");
-                        }
+                        }*/
                         // Create the row record on the fly now.
                         RowRecord rowRec = new RowRecord(cval.Row);
                         _sheet.AddRow(rowRec);
