e627b7071066a1bcc15c467126b2f4ee2691c17d
antony liu <sun_apollo@yeah.net>
2019-11-09 21:46:56 +0800

    POI：bug 57840: add Table interface for Structured References. Patch from Daniel Livshen and Greg Woolsey.

---
+++ main/SS/UserModel/Table.cs
@@ -0,0 +1,78 @@
+/* ====================================================================
+   Licensed to the Apache Software Foundation (ASF) under one or more
+   contributor license agreements.  See the NOTICE file distributed with
+   this work for Additional information regarding copyright ownership.
+   The ASF licenses this file to You under the Apache License, Version 2.0
+   (the "License"); you may not use this file except in compliance with
+   the License.  You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+==================================================================== */
+
+namespace NPOI.SS.UserModel
+{
+    using System;
+    using System.Text.RegularExpressions;
+
+
+
+    /**
+     * XSSF Only!
+     * High level abstraction of table in a workbook.
+     */
+    public interface ITable
+    {
+        /**
+         *  Get the top-left column index relative to the sheet
+         * @return table start column index on sheet
+         */
+        int StartColIndex { get; }
+        /**
+         *  Get the top-left row index on the sheet
+         * @return table start row index on sheet
+         */
+        int StartRowIndex { get; }
+        /**
+         *  Get the bottom-right column index on the sheet
+         * @return table end column index on sheet
+         */
+        int EndColIndex { get; }
+        /**
+         *  Get the bottom-right row index
+         * @return table end row index on sheet
+         */
+        int EndRowIndex { get; }
+        /**
+         * Get the name of the table.
+         * @return table name
+         */
+        String Name { get; }
+
+        /**
+         * Returns the index of a given named column in the table (names are case insensitive in XSSF).
+         * Note this list is lazily loaded and cached for performance. 
+         * Changes to the underlying table structure are not reflected in later calls
+         * unless <code>XSSFTable.UpdateHeaders()</code> is called to reset the cache.
+         * @param columnHeader the column header name to Get the table column index of
+         * @return column index corresponding to <code>columnHeader</code>
+         */
+        int FindColumnIndex(String columnHeader);
+        /**
+         * Returns the sheet name that the table belongs to.
+         */
+        String SheetName { get; }
+        /**
+         * Returns true iff the table has a 'Totals' row
+         */
+        bool IsHasTotalsRow { get; }
+
+
+    }
+
+}
