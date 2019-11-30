03147c82603ab6b4420a96e9d9fd8b0e69bf4a3f
antony liu <sun_apollo@yeah.net>
2019-11-30 22:02:07 +0800

    POI：Refactor out some of the common FormulaEvaluator logic from HSSF to a base class (XSSF coming shortly)

--- main/HSSF/UserModel/HSSFFormulaEvaluator.cs
+++ main/HSSF/UserModel/HSSFFormulaEvaluator.cs
@@ -30,9 +30,8 @@ namespace NPOI.HSSF.UserModel
      * @author Amol S. Deshmukh &lt; amolweb at ya hoo dot com &gt;
      * 
      */
-    public class HSSFFormulaEvaluator : IFormulaEvaluator, IWorkbookEvaluatorProvider
+    public class HSSFFormulaEvaluator : BaseFormulaEvaluator
     {
-        private WorkbookEvaluator _bookEvaluator;
         // params to lookup the right constructor using reflection
         private static Type[] VALUE_CONTRUCTOR_CLASS_ARRAY = new Type[] { typeof(Ptg) };
 
@@ -61,12 +60,11 @@ namespace NPOI.HSSF.UserModel
 
         protected IRow row;
         protected ISheet sheet;
-        protected IWorkbook workbook;
+        protected IWorkbook _book;
 
         public HSSFFormulaEvaluator(IWorkbook workbook)
             : this(workbook, null)
         {
-            this.workbook = workbook;
         }
         /**
          * @param stabilityClassifier used to optimise caching performance. Pass <code>null</code>
@@ -76,7 +74,6 @@ namespace NPOI.HSSF.UserModel
         public HSSFFormulaEvaluator(IWorkbook workbook, IStabilityClassifier stabilityClassifier)
             : this(workbook, stabilityClassifier, null)
         {
-
         }
 
 
@@ -85,8 +82,9 @@ namespace NPOI.HSSF.UserModel
          * @param udfFinder pass <code>null</code> for default (AnalysisToolPak only)
          */
         public HSSFFormulaEvaluator(IWorkbook workbook, IStabilityClassifier stabilityClassifier, UDFFinder udfFinder)
+            : base(new WorkbookEvaluator(HSSFEvaluationWorkbook.Create(workbook), stabilityClassifier, udfFinder))
         {
-            _bookEvaluator = new WorkbookEvaluator(HSSFEvaluationWorkbook.Create(workbook), stabilityClassifier, udfFinder);
+            _book = workbook;
         }
 
         /**
@@ -100,52 +98,6 @@ namespace NPOI.HSSF.UserModel
             return new HSSFFormulaEvaluator(workbook, stabilityClassifier, udfFinder);
         }
 
-
-        private static void SetCellType(ICell cell, CellValue cv)
-        {
-            CellType cellType = cv.CellType;
-            switch (cellType)
-            {
-                case CellType.Boolean:
-                case CellType.Error:
-                case CellType.Numeric:
-                case CellType.String:
-                    cell.SetCellType(cellType);
-                    return;
-                case CellType.Blank:
-                    // never happens - blanks eventually get translated to zero
-                    break;
-                case CellType.Formula:
-                    // this will never happen, we have already evaluated the formula
-                    break;
-            }
-            throw new InvalidOperationException("Unexpected cell value type (" + cellType + ")");
-        }
-        private static void SetCellValue(ICell cell, CellValue cv)
-        {
-            CellType cellType = cv.CellType;
-            switch (cellType)
-            {
-                case CellType.Boolean:
-                    cell.SetCellValue(cv.BooleanValue);
-                    break;
-                case CellType.Error:
-                    cell.SetCellErrorValue((byte)cv.ErrorValue);
-                    break;
-                case CellType.Numeric:
-                    cell.SetCellValue(cv.NumberValue);
-                    break;
-                case CellType.String:
-                    cell.SetCellValue(new HSSFRichTextString(cv.StringValue));
-                    break;
-                //case CellType.Blank:
-                //// never happens - blanks eventually get translated to zero
-                //case CellType.Formula:
-                //// this will never happen, we have already evaluated the formula
-                default:
-                    throw new InvalidOperationException("Unexpected cell value type (" + cellType + ")");
-            }
-        }
         /**
          * Coordinates several formula evaluators together so that formulas that involve external
          * references can be evaluated.
@@ -155,86 +107,21 @@ namespace NPOI.HSSF.UserModel
          */
         public static void SetupEnvironment(String[] workbookNames, HSSFFormulaEvaluator[] evaluators)
         {
-            WorkbookEvaluator[] wbEvals = new WorkbookEvaluator[evaluators.Length];
-            for (int i = 0; i < wbEvals.Length; i++)
-            {
-                wbEvals[i] = evaluators[i]._bookEvaluator;
-            }
-            CollaboratingWorkbooksEnvironment.Setup(workbookNames, wbEvals);
+            BaseFormulaEvaluator.SetupEnvironment(workbookNames, evaluators);
         }
 
-        public void SetupReferencedWorkbooks(Dictionary<String, IFormulaEvaluator> evaluators)
+        public override void SetupReferencedWorkbooks(Dictionary<String, IFormulaEvaluator> evaluators)
         {
             CollaboratingWorkbooksEnvironment.SetupFormulaEvaluator(evaluators);
         }
 
-        public WorkbookEvaluator GetWorkbookEvaluator()
-        {
-            return _bookEvaluator;
-        }
-        /**
-         * If cell Contains a formula, the formula is Evaluated and returned,
-         * else the CellValue simply copies the appropriate cell value from
-         * the cell and also its cell type. This method should be preferred over
-         * EvaluateInCell() when the call should not modify the contents of the
-         * original cell. 
-         * @param cell
-         */
-        /**
-         * If cell contains a formula, the formula is evaluated and returned,
-         * else the CellValue simply copies the appropriate cell value from
-         * the cell and also its cell type. This method should be preferred over
-         * evaluateInCell() when the call should not modify the contents of the
-         * original cell.
-         * 
-         * @param cell may be <c>null</c> signifying that the cell is not present (or blank)
-         * @return <c>null</c> if the supplied cell is <c>null</c> or blank
-         */
-        public CellValue Evaluate(ICell cell)
-        {
-            if (cell == null)
-            {
-                return null;
-            }
-
-            switch (cell.CellType)
-            {
-                case CellType.Boolean:
-                    return CellValue.ValueOf(cell.BooleanCellValue);
-                case CellType.Error:
-                    return CellValue.GetError(cell.ErrorCellValue);
-                case CellType.Formula:
-                    return EvaluateFormulaCellValue(cell);
-                case CellType.Numeric:
-                    return new CellValue(cell.NumericCellValue);
-                case CellType.String:
-                    return new CellValue(cell.RichStringCellValue.String);
-                case CellType.Blank:
-                    return null;
-            }
-            throw new InvalidOperationException("Bad cell type (" + cell.CellType + ")");
-        }
-
-
-        /**
-         * Should be called whenever there are major changes (e.g. moving sheets) to input cells
-         * in the evaluated workbook.  If performance is not critical, a single call to this method
-         * may be used instead of many specific calls to the notify~ methods.
-         *  
-         * Failure to call this method after changing cell values will cause incorrect behaviour
-         * of the evaluate~ methods of this class
-         */
-        public void ClearAllCachedResultValues()
-        {
-            _bookEvaluator.ClearAllCachedResultValues();
-        }
         /**
          * Should be called to tell the cell value cache that the specified (value or formula) cell 
          * has changed.
          * Failure to call this method after changing cell values will cause incorrect behaviour
          * of the evaluate~ methods of this class
          */
-        public void NotifyUpdateCell(ICell cell)
+        public override void NotifyUpdateCell(ICell cell)
         {
             _bookEvaluator.NotifyUpdateCell(new HSSFEvaluationCell(cell));
         }
@@ -244,7 +131,7 @@ namespace NPOI.HSSF.UserModel
          * Failure to call this method after changing cell values will cause incorrect behaviour
          * of the evaluate~ methods of this class
          */
-        public void NotifyDeleteCell(ICell cell)
+        public override void NotifyDeleteCell(ICell cell)
         {
             _bookEvaluator.NotifyDeleteCell(new HSSFEvaluationCell(cell));
         }
@@ -255,45 +142,16 @@ namespace NPOI.HSSF.UserModel
          * Failure to call this method after changing cell values will cause incorrect behaviour
          * of the evaluate~ methods of this class
          */
-        public void NotifySetFormula(ICell cell)
+        public override void NotifySetFormula(ICell cell)
         {
             _bookEvaluator.NotifyUpdateCell(new HSSFEvaluationCell(cell));
         }
 
         /**
-         * If cell Contains formula, it Evaluates the formula,
-         *  and saves the result of the formula. The cell
-         *  remains as a formula cell.
-         * Else if cell does not contain formula, this method leaves
-         *  the cell UnChanged. 
-         * Note that the type of the formula result is returned,
-         *  so you know what kind of value is also stored with
-         *  the formula. 
-         * <pre>
-         * int EvaluatedCellType = evaluator.EvaluateFormulaCell(cell);
-         * </pre>
-         * Be aware that your cell will hold both the formula,
-         *  and the result. If you want the cell Replaced with
-         *  the result of the formula, use {@link #EvaluateInCell(HSSFCell)}
-         * @param cell The cell to Evaluate
-         * @return The type of the formula result (the cell's type remains as CellType.Formula however)
-         */
-        public CellType EvaluateFormulaCell(ICell cell)
-        {
-            if (cell == null || cell.CellType != CellType.Formula)
-            {
-                return CellType.Unknown;
-            }
-            CellValue cv = EvaluateFormulaCellValue(cell);
-            // cell remains a formula cell, but the cached value is changed
-            SetCellValue(cell, cv);
-            return cv.CellType;
-        }
-        /**
          * Returns a CellValue wrapper around the supplied ValueEval instance.
          * @param cell
          */
-        private CellValue EvaluateFormulaCellValue(ICell cell)
+        protected override CellValue EvaluateFormulaCellValue(ICell cell)
         {
             ValueEval eval = _bookEvaluator.Evaluate(new HSSFEvaluationCell((HSSFCell)cell));
             if (eval is BoolEval)
@@ -333,7 +191,7 @@ namespace NPOI.HSSF.UserModel
          *  value computed for you, use {@link #EvaluateFormulaCell(HSSFCell)}
          * @param cell
          */
-        public ICell EvaluateInCell(ICell cell)
+        public override ICell EvaluateInCell(ICell cell)
         {
             if (cell == null)
             {
@@ -364,22 +222,22 @@ namespace NPOI.HSSF.UserModel
             EvaluateAllFormulaCells(wb, new HSSFFormulaEvaluator(wb));
         }
         /**
-           * Loops over all cells in all sheets of the supplied
-           *  workbook.
-           * For cells that contain formulas, their formulas are
-           *  evaluated, and the results are saved. These cells
-           *  remain as formula cells.
-           * For cells that do not contain formulas, no changes
-           *  are made.
-           * This is a helpful wrapper around looping over all
-           *  cells, and calling evaluateFormulaCell on each one.
-           */
-        public static void EvaluateAllFormulaCells(IWorkbook wb)
+         * Loops over all cells in all sheets of the supplied
+         *  workbook.
+         * For cells that contain formulas, their formulas are
+         *  evaluated, and the results are saved. These cells
+         *  remain as formula cells.
+         * For cells that do not contain formulas, no changes
+         *  are made.
+         * This is a helpful wrapper around looping over all
+         *  cells, and calling evaluateFormulaCell on each one.
+         */
+        public new static void EvaluateAllFormulaCells(IWorkbook wb)
         {
             IFormulaEvaluator evaluator = wb.GetCreationHelper().CreateFormulaEvaluator();
             EvaluateAllFormulaCells(wb, evaluator);
         }
-        private static void EvaluateAllFormulaCells(IWorkbook wb, IFormulaEvaluator evaluator)
+        private new static void EvaluateAllFormulaCells(IWorkbook wb, IFormulaEvaluator evaluator)
         {
             for (int i = 0; i < wb.NumberOfSheets; i++)
             {
@@ -399,40 +257,9 @@ namespace NPOI.HSSF.UserModel
             }
         }
 
-
-        public void EvaluateAll()
+        public override void EvaluateAll()
         {
-            HSSFFormulaEvaluator.EvaluateAllFormulaCells(workbook, this);
+            HSSFFormulaEvaluator.EvaluateAllFormulaCells(_book, this);
         }
-
-        /**
-         * Whether to ignore missing references to external workbooks and
-         * use cached formula results in the main workbook instead.
-         * <p>
-         * In some cases exetrnal workbooks referenced by formulas in the main workbook are not avaiable.
-         * With this method you can control how POI handles such missing references:
-         * <ul>
-         *     <li>by default ignoreMissingWorkbooks=false and POI throws {@link org.apache.poi.ss.formula.CollaboratingWorkbooksEnvironment.WorkbookNotFoundException}
-         *     if an external reference cannot be resolved</li>
-         *     <li>if ignoreMissingWorkbooks=true then POI uses cached formula result
-         *     that already exists in the main workbook</li>
-         * </ul>
-         *</p>
-         * @param ignore whether to ignore missing references to external workbooks
-         */
-        public bool IgnoreMissingWorkbooks
-        {
-            get { return _bookEvaluator.IgnoreMissingWorkbooks; }
-            set { _bookEvaluator.IgnoreMissingWorkbooks = value; }
-        }
-
-        /** {@inheritDoc} */
-        public bool DebugEvaluationOutputForNextEval
-        {
-            get { return _bookEvaluator.DebugEvaluationOutputForNextEval; }
-            set { _bookEvaluator.DebugEvaluationOutputForNextEval = value; }
-            
-        }
-
     }
 }
