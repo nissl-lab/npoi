b488d16aad2db67cc521e07281c4614fe28534c2
Tony Qu <tonyqus@gmail.com>
2020-03-03 22:32:29 +0800

    更新TimeSheetDemo和BusinessPlan

--- examples/ss/TimeSheetDemo/Program.cs
+++ examples/ss/TimeSheetDemo/Program.cs
@@ -61,15 +61,15 @@ namespace TimeSheetDemo
             ISheet sheet = workbook.CreateSheet("Timesheet");
             IPrintSetup printSetup = sheet.PrintSetup;
             printSetup.Landscape = true;
-            sheet.FitToPage=(true);
-            sheet.HorizontallyCenter=(true);
+            sheet.FitToPage = (true);
+            sheet.HorizontallyCenter = (true);
 
             //title row
             IRow titleRow = sheet.CreateRow(0);
-            titleRow.HeightInPoints=(45);
+            titleRow.HeightInPoints = (45);
             ICell titleCell = titleRow.CreateCell(0);
             titleCell.SetCellValue("Weekly Timesheet");
-            titleCell.CellStyle= (styles["title"]);
+            titleCell.CellStyle = (styles["title"]);
             sheet.AddMergedRegion(CellRangeAddress.ValueOf("$A$1:$L$1"));
 
             //header row
@@ -118,13 +118,14 @@ namespace TimeSheetDemo
 
             ICell cell2 = sumRow.CreateCell(1);
             cell2.SetCellValue("Total Hrs:");
-            cell2.CellStyle=(styles["formula"]);
+            cell2.CellStyle = (styles["formula"]);
 
-            for (int j = 2; j < 12; j++) {
+            for (int j = 2; j < 12; j++)
+            {
                 ICell cell = sumRow.CreateCell(j);
                 String reference = (char)('A' + j) + "3:" + (char)('A' + j) + "12";
                 cell.CellFormula = ("SUM(" + reference + ")");
-                if(j >= 9)
+                if (j >= 9)
                     cell.CellStyle = (styles["formula_2"]);
                 else
                     cell.CellStyle = (styles["formula"]);
@@ -138,7 +139,7 @@ namespace TimeSheetDemo
             cell3.CellStyle = styles["formula"];
             cell3 = sumRow.CreateCell(1);
             cell3.CellFormula = ("L13");
-            cell3.CellStyle=styles["formula_2"];
+            cell3.CellStyle = styles["formula_2"];
             sumRow = sheet.CreateRow(rownum++);
             sumRow.HeightInPoints = (25);
             cell3 = sumRow.CreateCell(0);
@@ -148,32 +149,33 @@ namespace TimeSheetDemo
             cell3.CellFormula = ("K13");
             cell3.CellStyle = styles["formula_2"];
 
-                    //set sample data
+            //set sample data
             for (int i = 0; i < sample_data.GetLength(0); i++)
             {
                 IRow row = sheet.GetRow(2 + i);
                 for (int j = 0; j < sample_data.GetLength(1); j++)
                 {
-                    if (sample_data[i,j] == null)
+                    if (sample_data[i, j] == null)
                         continue;
 
-                    if (sample_data[i,j] is String)
+                    if (sample_data[i, j] is String)
                     {
-                        row.GetCell(j).SetCellValue((String)sample_data[i,j]);
+                        row.GetCell(j).SetCellValue((String)sample_data[i, j]);
                     }
                     else
                     {
-                        row.GetCell(j).SetCellValue((Double)sample_data[i,j]);
+                        row.GetCell(j).SetCellValue((Double)sample_data[i, j]);
                     }
                 }
             }
 
-                    //finally set column widths, the width is measured in units of 1/256th of a character width
-        sheet.SetColumnWidth(0, 30*256); //30 characters wide
-        for (int i = 2; i < 9; i++) {
-            sheet.SetColumnWidth(i, 6*256);  //6 characters wide
-        }
-        sheet.SetColumnWidth(10, 10*256); //10 characters wide
+            //finally set column widths, the width is measured in units of 1/256th of a character width
+            sheet.SetColumnWidth(0, 30 * 256); //30 characters wide
+            for (int i = 2; i < 9; i++)
+            {
+                sheet.SetColumnWidth(i, 6 * 256);  //6 characters wide
+            }
+            sheet.SetColumnWidth(10, 10 * 256); //10 characters wide
 
 
             WriteToFile();
