13d5db009c33412dda6940c03002c121543e1301
Tony Qu <tonyqus@gmail.com>
2021-05-31 07:30:14 +0800

    The default value of PatternType should be null instead of ST_PatternType.none
    fix #186

--- OpenXmlFormats/Spreadsheet/Styles/CT_PatternFill.cs
+++ OpenXmlFormats/Spreadsheet/Styles/CT_PatternFill.cs
@@ -16,7 +16,7 @@ namespace NPOI.OpenXmlFormats.Spreadsheet
 
         private CT_Color bgColorField = null;
 
-        private ST_PatternType patternTypeField = ST_PatternType.none;
+        private ST_PatternType? patternTypeField=null;
 
         public bool IsSetPatternType()
         {
@@ -67,7 +67,8 @@ namespace NPOI.OpenXmlFormats.Spreadsheet
         internal void Write(StreamWriter sw, string nodeName)
         {
             sw.Write(string.Format("<{0}", nodeName));
-            XmlHelper.WriteAttribute(sw, "patternType", this.patternType.ToString());
+            if(patternType!=null)
+                XmlHelper.WriteAttribute(sw, "patternType", this.patternType.ToString());
             if (this.fgColor == null && this.bgColor == null)
             {
                 sw.Write("/>");
@@ -120,7 +121,7 @@ namespace NPOI.OpenXmlFormats.Spreadsheet
         }
 
         [XmlAttribute]
-        public ST_PatternType patternType
+        public ST_PatternType? patternType
         {
             get
             {
