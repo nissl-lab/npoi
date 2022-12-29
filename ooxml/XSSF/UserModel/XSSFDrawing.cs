668b919e836aa63509b24a7296718ef0786ba802
Artem Koloskov <artem@ironsoftware.com>
2022-12-29 10:57:50 +0700

    Add private GetNewChartNumber to XSSFDrawing
    The method checks for existing chat names, and gets the firtst chart number that isnt in use yet.

--- ooxml/XSSF/UserModel/XSSFDrawing.cs
+++ ooxml/XSSF/UserModel/XSSFDrawing.cs
@@ -26,7 +26,8 @@ using System.Xml;
 using NPOI.OpenXmlFormats.Dml;
 using NPOI.SS.Util;
 using NPOI.Util;
-
+using System.Text.RegularExpressions;
+using System.Linq;
 
 namespace NPOI.XSSF.UserModel
 {
@@ -170,8 +171,7 @@ namespace NPOI.XSSF.UserModel
         /// <returns>the newly created chart</returns>
         public IChart CreateChart(IClientAnchor anchor)
         {
-            int chartNumber = GetPackagePart().Package.
-                GetPartsByContentType(XSSFRelation.CHART.ContentType).Count + 1;
+            int chartNumber = GetNewChartNumber();
 
             RelationPart rp = CreateRelationship(
             XSSFRelation.CHART, XSSFFactory.GetInstance(), chartNumber, false);
@@ -182,6 +182,32 @@ namespace NPOI.XSSF.UserModel
             frame.SetChart(chart, chartRelId);
 
             return chart;
+        }
+
+        private int GetNewChartNumber()
+        {
+            List<PackagePart> existingCharts = GetPackagePart().Package.
+                GetPartsByContentType(XSSFRelation.CHART.ContentType);
+            HashSet<int> numbers = new HashSet<int>();
+
+            foreach (PackagePart chart in existingCharts)
+            {
+                var match = Regex.Match(chart.PartName.Name, @"\d+");
+
+                if (match.Success)
+                {
+                    numbers.Add(int.Parse(match.Value));
+                }
+            }
+
+            var newChartNumber = 1;
+
+            while (numbers.Contains(newChartNumber))
+            {
+                newChartNumber++;
+            }
+
+            return newChartNumber;
         }
 
         //public XSSFChart CreateChart(IClientAnchor anchor)
