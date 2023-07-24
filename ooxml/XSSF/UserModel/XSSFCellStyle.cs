7a072729e4acec65a13bc042b3aac74e9bd85158
Andrey Bykiev <bykiev.andrei@mail.ru>
2023-07-24 17:43:18 +0300

    Modify IsHidden check

--- ooxml/XSSF/UserModel/XSSFCellStyle.cs
+++ ooxml/XSSF/UserModel/XSSFCellStyle.cs
@@ -695,11 +695,7 @@ namespace NPOI.XSSF.UserModel
         {
             get
             {
-                if (!_cellXf.IsSetProtection() || !_cellXf.protection.IsSetHidden())
-                {
-                    return false;
-                }
-                return _cellXf.protection.hidden;
+                return _cellXf.IsSetProtection() && _cellXf.protection.IsSetHidden() && _cellXf.protection.hidden;
             }
             set
             {
