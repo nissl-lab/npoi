61176f1639b656f6d15f4b9858bfd2b93be3fbbf
Artem Koloskov <artem@ironsoftware.com>
2023-08-24 18:53:04 +0700

    upstr: Add null checks to XSSFSheet.RemoveHyperlink method

--- ooxml/XSSF/UserModel/XSSFSheet.cs
+++ ooxml/XSSF/UserModel/XSSFSheet.cs
@@ -3293,7 +3293,14 @@ namespace NPOI.XSSF.UserModel
                 XSSFHyperlink hyperlink = hyperlinks[index];
                 if (hyperlink.CellRef.Equals(ref1))
                 {
-                    worksheet.hyperlinks.hyperlink.Remove(hyperlink.GetCTHyperlink());
+                    if (worksheet != null
+                        && worksheet.hyperlinks != null
+                        && worksheet.hyperlinks.hyperlink != null
+                        && worksheet.hyperlinks.hyperlink.Contains(hyperlink.GetCTHyperlink()))
+                    {
+                        worksheet.hyperlinks.hyperlink.Remove(hyperlink.GetCTHyperlink());
+                    }
+
                     hyperlinks.RemoveAt(index);
                     return;
                 }
