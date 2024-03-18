03cfe7dd6af1676787c708c50078fefd294a63ef
Antony Liu <sun_apollo@yeah.net>
2024-03-18 10:32:45 +0800

    poi: Add tests from bug 45353 to show that this now works fine

--- testcases/main/HSSF/UserModel/TestBugs.cs
+++ testcases/main/HSSF/UserModel/TestBugs.cs
@@ -3481,10 +3481,24 @@ namespace TestCases.HSSF.UserModel
             wb.Close();
         }
 
+        [Test]
+        public void Test45353a()
+        {
+            IWorkbook wb = HSSFTestDataSamples.OpenSampleWorkbook("named-cell-in-formula-test.xls");
+            wb.GetCreationHelper().CreateFormulaEvaluator().EvaluateAll();
+            wb.Close();
+        }
+
+        [Test]
+        public void Test45353b()
+        {
+            IWorkbook wb = HSSFTestDataSamples.OpenSampleWorkbook("named-cell-test.xls");
+            wb.GetCreationHelper().CreateFormulaEvaluator().EvaluateAll();
+            wb.Close();
+        }
 
         // follow https://svn.apache.org/viewvc?view=revision&revision=1896552 to write a unit test for this fix.
-        [Test]
-        public void Test52447()
+        [Test]        public void Test52447()
         {
             IWorkbook wb = null;
             try
