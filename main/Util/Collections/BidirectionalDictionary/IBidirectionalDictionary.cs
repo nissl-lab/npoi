62b687227a9f20e4a3ede11e3d3ed8663c3d1c91
Antony Liu <sun_apollo@yeah.net>
2024-03-19 09:17:44 +0800

    Add BidirectionalDictionary for HPSF improvement

---
+++ main/Util/Collections/BidirectionalDictionary/IBidirectionalDictionary.cs
@@ -0,0 +1,10 @@
+﻿namespace System.Collections.Generic
+{
+    public interface IBidirectionalDictionary<TKey, TValue> : IDictionary<TKey, TValue>
+        where TKey : notnull
+        where TValue : notnull
+    {
+        public IBidirectionalDictionary<TValue, TKey> Inverse { get; }
+        public bool ContainsValue(TValue value);
+    }
+}
