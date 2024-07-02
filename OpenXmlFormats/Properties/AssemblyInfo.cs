1c3e4bd1769bb4ebe25f0e0ea3536e28a7f4979d
Marko Lahma <marko.lahma@gmail.com>
2024-07-02 09:48:31 +0300

    Use Directory.Build.props as single source of version information

--- OpenXmlFormats/Properties/AssemblyInfo.cs
+++ OpenXmlFormats/Properties/AssemblyInfo.cs
@@ -1,41 +1,6 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+﻿using System.Runtime.CompilerServices;
 using System.Security;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("NPOI OpenXmlFormats")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Nissl")]
-[assembly: AssemblyProduct("NPOI")]
-[assembly: AssemblyCopyright("Apache 2.0")]
-[assembly: AssemblyTrademark("NPOI")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("c67494d1-2eff-42d6-b0e5-063c750e4f2f")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.7.1.0")]
-[assembly: AssemblyFileVersion("2.7.1.0")]
-[assembly: AssemblyInformationalVersion("2.0.0.0")]
 #if NETSTANDARD2_1 || NET6_0_OR_GREATER || NETSTANDARD2_0 || NET40
 [assembly: AllowPartiallyTrustedCallers]
 #endif
