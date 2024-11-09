3dc47a85aed8dc7c2daf33c9093a97b4d85e5cd4
Tony Qu <tonyqus@gmail.com>
2024-11-09 08:54:17 +0800

    fix #1370

---
+++ testcases/ooxml/XSSF/UserModel/TestCloneSheet.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System.IO;
+
+namespace NPOI.XSSF.UserModel
+{
+    [TestFixture]
+    public class TestCloneSheet
+    {
+        [Test]
+        public void TestCloneHyperlink()
+        {
+            using(var workbook = XSSFTestDataSamples.OpenSampleWorkbook("1370_clonesheet_withhyperlink.xlsx"))
+            {
+                workbook.CloneSheet(0,"Sheet2");
+            }
+        }
+    }
+}
