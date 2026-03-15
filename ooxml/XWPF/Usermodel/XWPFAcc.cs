9bf682d661a9e64d779a8b795c649169e0479fa5
Tony Qu <tonyqus@gmail.com>
2026-03-15 11:44:39 +0800

    remove ;

--- ooxml/XWPF/Usermodel/XWPFAcc.cs
+++ ooxml/XWPF/Usermodel/XWPFAcc.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using NPOI.OpenXmlFormats.Shared;;
+using NPOI.OpenXmlFormats.Shared;
 
 namespace NPOI.XWPF.UserModel
 {
