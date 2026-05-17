8c742cea08d0cba6a7149f28d98973dbb31e3257
stevenaw <sweerdenburg@gmail.com>
2026-05-16 20:31:16 -0400

    Optimize Number.BitCount() on newer runtimes

---
+++ testcases/main/Util/TestNumber.cs
@@ -0,0 +1,16 @@
+﻿using NPOI.Util;
+using NUnit.Framework;
+
+namespace TestCases.Util
+{
+    internal class TestNumber
+    {
+        [TestCase(1, ExpectedResult = 1)]
+        [TestCase(16, ExpectedResult = 1)]
+        [TestCase(31, ExpectedResult = 5)]
+        [TestCase(-1, ExpectedResult = 32)]
+        [TestCase(int.MaxValue, ExpectedResult = 31)]
+        [TestCase(int.MinValue, ExpectedResult = 1)]
+        public static int TestBitCount(int value) => Number.BitCount(value);
+    }
+}
