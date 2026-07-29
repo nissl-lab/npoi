de0b90550ece5ea68c2bd786b81242bfb64a07ec
Tony Qu <tonyqus@gmail.com>
2026-07-29 17:37:07 +0800

    Implement SXSSFWorkbook.ForceFormulaRecalculation

--- ooxml/XSSF/Streaming/SXSSFWorkbook.cs
+++ ooxml/XSSF/Streaming/SXSSFWorkbook.cs
@@ -1066,11 +1066,25 @@ namespace NPOI.XSSF.Streaming
         {
             get
             {
-                throw new NotImplementedException();
+                bool forcerecalc=true;
+                for(int i = 0; i < this.NumberOfSheets; i++)
+                {
+                    var sheet = this.GetSheetAt(i);
+                    if(!sheet.ForceFormulaRecalculation)
+                    {
+                        forcerecalc = false;
+                        break;
+                    }
+                }
+                return forcerecalc;
             }
             set
             {
-                throw new NotImplementedException();
+                for(int i = 0; i < this.NumberOfSheets; i++)
+                {
+                    var sheet = this.GetSheetAt(i);
+                    sheet.ForceFormulaRecalculation = value;
+                }
             }
         }
     }
